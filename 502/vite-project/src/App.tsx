845e8fc3fd6038c87e30114e0d05b0ff48dec1da
Dokyu Han <dokyuhan@Dokyus-MacBook-Pro.local>
2025-04-01 19:50:47 -0600

    ultimos cambios para la tarea

--- 502/vite-project/src/App.tsx
+++ 502/vite-project/src/App.tsx
@@ -2,10 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { ArrowFunction } from './class_1/ArrowFunction.tsx'
-import { Destructuring } from './class_1/Destruction.tsx'
-import { TemplateLiteral } from './class_1/TemplateLiteral.tsx'
-import { GreetExample } from './class_1/GreetExample.tsx'
+import { ArrowFunction } from './class1/ArrowFunction.tsx'
+import { Destructuring } from './class1/Destruction.tsx'
+import { TemplateLiteral } from './class1/TemplateLiteral.tsx'
+import { GreetExample } from './class1/GreetExample.tsx'
 
 function App() {
   const [name] = useState("Do Kyu");
