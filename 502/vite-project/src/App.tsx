feab0e24d89fedd84df0f72d1d5be54c8d5ea2dc
rafaelblanga <A01781442@tec.mx>
2025-04-01 21:14:43 -0600

    Agrega estructura de login para class2 y modifica App.tsx

--- 502/vite-project/src/App.tsx
+++ 502/vite-project/src/App.tsx
@@ -25,6 +25,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <a href="./class2/A01781442/index.html">
+      <button>
+     Ir a Login
+      </button>
+      </a>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ 502/vite-project/src/class2/A01781442/login.tsx
@@ -0,0 +1,13 @@
+// src/class2/A01781442/Login.tsx
+import React from 'react'
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h2>Página de Login</h2>
+      <p>Bienvenido al login</p>
+    </div>
+  )
+}
+
+export default Login
