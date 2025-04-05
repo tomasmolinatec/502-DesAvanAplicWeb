32d944203320bddbc08478f1bb4557f987195813
zuamaya <a01784238@tec.mx>
2025-04-05 12:02:13 -0600

    finished class 1 task and add menu class page

---
+++ 502/vite-project/src/class1/A01784238/menu/MainMenu.tsx
@@ -0,0 +1,10 @@
+const MainMenu = () => {
+    return (
+        <div>
+            <h1>Menú principal de actividades</h1>
+            <a href="/src/class1/A01784238/index.html">Clase 1</a>
+        </div>
+    );
+};
+
+export default MainMenu;
