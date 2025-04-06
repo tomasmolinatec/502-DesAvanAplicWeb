d4efaf952de0e6dba08ffe01be011947475115bf
Imanol Santisteban <a01783637@tec.mx>
2025-04-06 11:49:27 -0600

    feat:Tarea1 finalizada

---
+++ 502/vite-project/src/class1/A01783637/Homework_Index/App.tsx
@@ -0,0 +1,12 @@
+//Imanol Santisteban
+//A01783637
+import "/src/App.css"
+const  App=()=>{
+    return(
+        <div>
+            <a href="/src/class1/A01783637/index.html"> HomeWork1 </a>
+        </div>
+    );
+};
+
+export default App;
