1a00ae573296efee0c9149b1dba1a539dc5a58fc
Imanol Santisteban <a01783637@tec.mx>
2025-04-06 12:54:28 -0600

    Feature: Finished homework2 of milestone1

--- 502/vite-project/src/class1/A01783637/Homework_Index/App.tsx
+++ 502/vite-project/src/class1/A01783637/Homework_Index/App.tsx
@@ -3,9 +3,15 @@
 import "/src/App.css"
 const  App=()=>{
     return(
+        <>
         <div>
-            <a href="/src/class1/A01783637/index.html"> HomeWork1 </a>
+            <a href="/src/class1/A01783637/index.html"> ~HomeWork1~ </a>
         </div>
+        <br />
+        <div>
+        <a href="/src/class2/A01783637/index.html"> ~HomeWork2~ </a>
+        </div>
+        </>
     );
 };
 
