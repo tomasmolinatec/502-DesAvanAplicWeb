f8b1d9a1b7b49410a8da7c8df5d63cf9a88ba70a
Leon Blanga <leonblanga@MacBook-Pro-de-Leon.local>
2025-04-08 20:30:42 -0600

    ajuste class1 para el milestone

--- 502/vite-project/src/class1/A01784008/App.tsx
+++ 502/vite-project/src/class1/A01784008/App.tsx
@@ -1,9 +1,11 @@
 import ES6Hw from "./ES6_Tarea.tsx";
+import LoginPage from "./LoginPage.tsx";
 import "/src/App.css"
 const  App=()=>{
     return(
         <div>
         <ES6Hw></ES6Hw>
+        <LoginPage></LoginPage>
         </div>
     );
 };
