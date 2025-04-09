62e9d7bfcfe58a2a2facee6155fcd47bdeb13993
Leon Blanga <leonblanga@MacBook-Pro-de-Leon.local>
2025-04-08 20:36:31 -0600

    Import Navbar

--- 502/vite-project/src/class1/A01784008/App.tsx
+++ 502/vite-project/src/class1/A01784008/App.tsx
@@ -1,5 +1,6 @@
 import ES6Hw from "./ES6_Tarea.tsx";
 import LoginPage from "./LoginPage.tsx";
+import Navbar from "../../class2/A01784008/components/NavbarLB.tsx";
 import "/src/App.css"
 import "../../class2/A01784008/App.tsx"
 
