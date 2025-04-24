5e7a612c6d8361e6423dd87fcc3c0b98d5c51c8c
Ivan Santiago Mendez <ivan.santiago@tec.mx>
2025-04-23 20:54:06 -0600

    fix(ISM): App.tsx cleanup

--- 502/vite-project/src/App.tsx
+++ 502/vite-project/src/App.tsx
@@ -3,10 +3,10 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      <h1>Vite + React</h1>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -15,30 +15,28 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => window.location.href = "/src/class3/A01782557/dashboard/index.html"}
-        >
-        Dashboard
-        </button>
 
-        <button onClick={() => navigateTo("/src/class3/A01781321/index.html")}>
-          Class projects
+      <div className="card">
+        <button onClick={() => window.location.href = "/src/class3/A01782557/dashboard/index.html"}>
+        A01782557 Dashboard
+        </button>
+        <button onClick={() => window.location.href = "/src/class3/A01781321/index.html"}>
+        A01781321 Class projects
         </button>
         <button>
-        <a href="/src/class1/A01784521/menu/menu.html">Activities index</a>
-        <a href="/src/menu/A01028038/index.html">
-          Menú
-        </a>
+            <a href="/src/class1/A01784521/menu/menu.html">A01784521 Activities index</a>
+        </button>
+        <button>
+            <a href="/src/menu/A01028038/index.html">
+            A01028038 Menú
+            </a>
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <p>
-          <a href='/src/class1/A01783637/Homework_Index/index.html'> Menu of Homeworks for Imanol Santisteban A01783637 </a>
+          <a href='/src/class1/A01783637/Homework_Index/index.html'>
+            Menu of Homeworks for Imanol Santisteban A01783637
+          </a>
           <a href="/src/class1/A01025119/webpage/A01025119.html" target="_blank" rel="noopener noreferrer" >
-            Pagina de Do Kyu
+          A01025119 Pagina de Do Kyu
           </a>
         </p>
       </div>
@@ -49,21 +47,19 @@ function App() {
         <a href='/src/class1/A01781442/index.html'>
           Rafael Blanga Hanono A01781442
         </a>
-        <a href="src/class1/A01784238/menu/index.html">Menú de José Manuel García Zumaya</a>
+        <a href="src/class1/A01784238/menu/index.html">A01784238 Menú de José Manuel García Zumaya</a>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <a href="/src/class1/A01784116/menu/index.html">
-        Clases Tomas Molina
-      <a/>
+      A01784116 Clases Tomas Molina
+      </a>
         <a
           href="/src/class1/A01782146/explorador/index.html"
           id="gabrieledid"
           className="gabrieledid"
         >
-          Ir a trabajos de Gabriel Edid A01782146
+          A01782146 Ir a trabajos de Gabriel Edid
         </a>
+    </div>
   )
 }
 
