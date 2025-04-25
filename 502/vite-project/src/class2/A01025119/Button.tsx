787356823ee3d43fdc05d9e9ac56791d898f239e
Francisco Urquizo <A01028786@tec.mx>
2025-04-25 16:34:03 -0600

    Eliminar código comentado

--- 502/vite-project/src/class2/A01025119/Button.tsx
+++ 502/vite-project/src/class2/A01025119/Button.tsx
@@ -1,21 +1,3 @@
-// import React from 'react';
-// import '../../class1/A01025119/webpage/main_design.css';
-
-// interface ButtonProps {
-//   label: string;
-//   onClick: () => void;
-// }
-
-// const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-//   return (
-//     <button className="main-button" onClick={onClick}>
-//       {label}
-//     </button>
-//   );
-// };
-
-// export default Button;
-
 import React from 'react';
 import '../../class1/A01025119/webpage/main_design.css';
 
