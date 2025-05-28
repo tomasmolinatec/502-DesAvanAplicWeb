5abe09e2e53df85040b114f0801da82de771d04d
GabrielEdid <gabrieledid28@gmail.com>
2025-05-27 18:44:10 -0600

    Una correcion para subirlo a la nube

--- 502/vite-project/src/App.tsx
+++ 502/vite-project/src/App.tsx
@@ -4,7 +4,6 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-
   return (
     <div>
       <h1>Vite + React</h1>
@@ -18,63 +17,69 @@ function App() {
       </div>
 
       <div className="card">
-        <button onClick={() => window.location.href = "/src/class3/A01782557/dashboard/index.html"}>
-        A01782557 Dashboard
+        <button
+          onClick={() =>
+            (window.location.href =
+              "/src/class3/A01782557/dashboard/index.html")
+          }
+        >
+          A01782557 Dashboard
         </button>
-        <button onClick={() => window.location.href = "/src/class3/A01781321/index.html"}>
-        A01781321 Class projects
+        <button
+          onClick={() =>
+            (window.location.href = "/src/class3/A01781321/index.html")
+          }
+        >
+          A01781321 Class projects
         </button>
         <button>
-            <a href="/src/class1/A01784521/menu/menu.html">A01784521 Activities index</a>
+          <a href="/src/class1/A01784521/menu/menu.html">
+            A01784521 Activities index
+          </a>
         </button>
         <button>
-            <a href="/src/menu/A01028038/index.html">
-            A01028038 Menú
-            </a>
+          <a href="/src/menu/A01028038/index.html">A01028038 Menú</a>
         </button>
         <p>
-          <a href='/src/class1/A01783637/Homework_Index/index.html'>
+          <a href="/src/class1/A01783637/Homework_Index/index.html">
             Menu of Homeworks for Imanol Santisteban A01783637
           </a>
-          <a href="/src/class1/A01025119/webpage/A01025119.html" target="_blank" rel="noopener noreferrer" >
-          A01025119 Pagina de Do Kyu
+          <a
+            href="/src/class1/A01025119/webpage/A01025119.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            A01025119 Pagina de Do Kyu
           </a>
         </p>
       </div>
       <div>
-        <a href='/src/class2/A01784008/index.html' target="_blank" rel="noopener noreferrer">
+        <a
+          href="/src/class2/A01784008/index.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Leon Blanga Hasbani A01784008
         </a>
-        <a href='/src/class1/A01781442/index.html'>
+        <a href="/src/class1/A01781442/index.html">
           Rafael Blanga Hanono A01781442
         </a>
-        <a href="src/class1/A01784238/menu/index.html">A01784238 Menú de José Manuel García Zumaya</a>
-      </div>
-<<<<<<< HEAD
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    <a
-      href="/src/class1/A01784116/menu/index.html"
-    >
-      Clases Tomas Molina
-    </a>
-  </>
-  );
-=======
-      <a href="/src/class1/A01784116/menu/index.html">
-      A01784116 Clases Tomas Molina
-      </a>
-        <a
-          href="/src/class1/A01782146/explorador/index.html"
-          id="gabrieledid"
-          className="gabrieledid"
-        >
-          A01782146 Ir a trabajos de Gabriel Edid
+        <a href="src/class1/A01784238/menu/index.html">
+          A01784238 Menú de José Manuel García Zumaya
         </a>
+      </div>
+      <a href="/src/class1/A01784116/menu/index.html">
+        A01784116 Clases Tomas Molina
+      </a>
+      <a
+        href="/src/class1/A01782146/explorador/index.html"
+        id="gabrieledid"
+        className="gabrieledid"
+      >
+        A01782146 Ir a trabajos de Gabriel Edid
+      </a>
     </div>
-  )
->>>>>>> 4731be77b95061e182abb5a5651d1bb560fbb1ae
+  );
 }
 
-export default App
+export default App;
