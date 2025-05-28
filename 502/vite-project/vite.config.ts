a206d64468cf6b94e03a6b65f268ced3a64d6c01
GabrielEdid <gabrieledid28@gmail.com>
2025-05-27 20:59:27 -0600

    Cambio

--- 502/vite-project/vite.config.ts
+++ 502/vite-project/vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "./", // routes relativas; crucial para despliegue en subdirectorios
+  build: {
+    outDir: "dist", // asegúrate de que sea 'dist'
+  },
+});
