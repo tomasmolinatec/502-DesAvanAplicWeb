bfa640af4b1776d3050c411a594e7d3837a84908
GabrielEdid <gabrieledid28@gmail.com>
2025-05-29 17:59:30 -0600

    cambios para la nube

--- 502/vite-project/vite.config.ts
+++ 502/vite-project/vite.config.ts
@@ -1,13 +1,28 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // 👉  usa una ruta BASE ABSOLUTA
-  base: "/", //  <──  esto evita el problema de /menu/assets/…
   build: {
-    outDir: "dist",
+    rollupOptions: {
+      input: {
+        app: path.resolve(__dirname, "index.html"),
+        menucelula: path.resolve(
+          __dirname,
+          "src/Celula_A01784116_Tomas/menu/index.html"
+        ),
+        class1: path.resolve(
+          __dirname,
+          "src/class1/A01784116/actividad/index.html"
+        ),
+        menu: path.resolve(__dirname, "src/class1/A01784116/menu/index.html"),
+        class2: path.resolve(__dirname, "src/class2/A01784116/index.html"),
+        class3: path.resolve(__dirname, "src/class3/A01784116/index.html"),
+      },
+    },
+    outDir: "dist", // carpeta de salida común
+    emptyOutDir: true,
   },
 });
