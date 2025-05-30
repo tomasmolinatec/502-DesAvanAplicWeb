7b5e437b422a7324dd51fce689dcfbdc5f970ea2
GabrielEdid <gabrieledid28@gmail.com>
2025-05-29 18:26:11 -0600

    cambios

--- 502/vite-project/vite.config.ts
+++ 502/vite-project/vite.config.ts
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  resolve: {
+    dedupe: ["react", "react-dom", "react-router-dom"], // ← evita dobles copias
+  },
   build: {
     outDir: "dist",
     emptyOutDir: true,
